5c62a0f8e1df6a181d7dca40c4a13bc274718db1
VarunK2801 <varunkapole19@gmail.com>
2023-10-03 18:34:41 +0530

    ecommerce complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import CartContextProvider from './Components/Context/CartContextProvider';
-import AuthContextProvider from './Components/Context/AuthContextProvider';
-
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import CartProvider from "./store/CartProvider";
+import { AuthProvider } from "./store/AuthContext";
+import { ProductsProvider } from "./store/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <CartContextProvider>
+  <AuthProvider>
+    <CartProvider>
+      <ProductsProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </CartContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
+      </ProductsProvider>
+    </CartProvider>
+  </AuthProvider>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
